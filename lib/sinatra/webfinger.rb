9188a0f7b99afcf07d432dc694ea293c94ff49d3
Eric Mill <eric@konklone.com>
2013-10-13 18:17:45 -0400

    Actual code to do it

--- lib/sinatra/webfinger.rb
+++ lib/sinatra/webfinger.rb
@@ -1,1 +1,48 @@
 require 'sinatra/base'
+require 'multi_json'
+
+# To use:
+#
+#   require 'sinatra/webfinger'
+#
+# In classic style:
+#
+#   webfinger(config)
+#
+# In modular style:
+#
+#
+
+module Sinatra
+
+  module Webfinger
+    def webfinger(config)
+      get '/.well-known/webfinger' do
+        halt 500 unless config
+        halt 400 unless (resource = params[:resource]).present?
+        halt 400 unless (uri = URI.parse(resource) rescue nil)
+        halt 404 unless uri.scheme == "acct" # acct only
+
+        no_scheme = resource.sub /^acct:/, ''
+        account = config['accounts'].find {|acct| acct['acct'] == no_scheme}
+        halt 404 unless account
+
+        response = {
+          subject: resource,
+          properties: account['properties'],
+          links: [] # fill in next
+        }
+        account['links'].each do |rel, href|
+          response[:links] << {rel: rel, href: href}
+        end
+
+        headers['Content-Type'] = 'application/jrd+json'
+        headers['Access-Control-Allow-Origin'] = "*"
+
+        MultiJson.encode response
+      end
+    end
+  end
+
+  register Webfinger
+end
